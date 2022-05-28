8d107639aca21145de1506d4994dc7a562052fee
rocapa18 <roberto.pavano@gmail.com>
2022-05-28 20:26:31 -0300

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import useQuiosco from "../hooks/useQuiosco"
+import Categoria from "./Categoria";
+const Sidebar = () => {
+  const {categorias} = useQuiosco();
+  return (
+    <>
+        <div className="mt-10">
+          <Image
+            width={300} 
+            height={100} 
+            src="/assets/img/logo.svg" 
+            alt="imagen logotipo" 
+          />
+        </div>
+        
+
+        <nav className="mt-5">
+          {categorias.map(categoria => (
+            <Categoria
+              key={categoria.id}
+              categoria = {categoria}
+            
+            />
+          ))}
+        </nav>
+    </>
+  )
+}
+
+export default Sidebar
